a286a476849e31f552835a352292f72c5b562e9d
KimBH <kimbh0437@gmail.com>
2026-07-03 23:42:08 +0900

    [feat] Level load System

---
+++ Assets/1.Scripts/ChartScripts/Chart.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.Json.Serialization;
+using UnityEngine;
+
+[Serializable]
+public struct NoteData
+{
+    public ulong Tick { get; private set; }
+
+    public NoteType Type { get; private set; }
+
+    public ulong Length { get; private set; }
+
+    public enum NoteType
+    {
+        Short,
+        Long,
+    }
+}
+
+[Serializable]
+public class ChartData
+{
+    public string Title { get; private set; }
+
+    public int BPM { get; private set; }
+
+    public NoteData[] Notes { get; private set;}
+
+    [JsonIgnore]
+    public AudioClip Clip { get; set; }
+}
